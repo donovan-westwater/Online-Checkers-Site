321d67eb0e828e5099cab4c6023fe0cacaa20364
Felix Luciano <felixluciano.a@gmail.com>
2021-04-14 17:33:06 -0400

    Added Google Login button

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {GoogleLogin} from 'react-google-login';
 const GOOGLE_OAUTH_CLIENT = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT;
 
 function Login() {
+    const onSuccess = (res)=>{
+        console.log('Login Success currentUser:',res.profileObj);
+    }
+    const onFailure = (res)=>{
+        console.log('[Login failed] res:',res);
+    }
     return(
-        <div>
-            <button onClick={()=>GOOGLE_OAUTH_CLIENT?console.log("Loaded"):console.log("Empty")}>here</button>
-        </div>
+        <GoogleLogin
+            clientId={GOOGLE_OAUTH_CLIENT}
+            buttonText="Login"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            style={{ marginTop: '100px' }}
+            isSignedIn={true}
+        />
     );
 }
 
