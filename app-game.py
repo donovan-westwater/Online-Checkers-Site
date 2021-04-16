e85076e85f5e03b6c3b7714471dd9978fb3be7d5
EC2 Default User <ec2-user@ip-172-31-51-68.ec2.internal>
2021-04-16 17:28:37 +0000

    I have the basic flask program working now. I am trying to get the style for the board working atm

--- app-game.py
+++ app-game.py
@@ -11,6 +11,6 @@ def index(filename):
 
 
 app.run(
-    host=os.getenv('IP', '127.0.0.1'),
+    host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
