0a94d4e3687761969bb7a1c638d287d663596dea
Charles Crider <ec2-user@ip-172-31-14-74.ec2.internal>
2021-04-21 00:21:07 +0000

    Linted except for 2 errors

--- src/Stats.js
+++ src/Stats.js
@@ -1,43 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
-
 function StatsComponent(props) {
-    const socket = props.socket;
-    const [stats, setStats] = useState([]);
-    
-    useEffect(() => {
+  const { socket } = props;
+  const [stats, setStats] = useState([]);
+
+  useEffect(() => {
     // Listener for app.py returning the userlist
     socket.on('requestAllStatsCallback', (data) => {
-      console.log('Socked recieved user stats');
-      console.log(data)
-      setStats(data)
+      setStats(data);
     });
-    }, []);
-    
-    function pingServerForUser(){
-        socket.emit('requestStats', { id: socket.id, usr: 1});
-    }
-    
-    function pingAllUserStats(){
-        socket.emit('requestAllStats', { id: socket.id})
-    }
-    
-    return (
-        <div>
-        <button onClick={pingAllUserStats}>Stats</button>
-        {stats.map((player, index) => (
-                <tr
-                  className={
+  }, []);
+
+  function pingServerForUser() {
+    socket.emit('requestStats', { id: socket.id, usr: 1 });
+  }
+
+  function pingAllUserStats() {
+    socket.emit('requestAllStats', { id: socket.id });
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={pingServerForUser()}>My Stats</button>
+      <button type="button" onClick={pingAllUserStats}>Stats</button>
+      {stats.map((player, index) => (
+        <tr
+          className={
                     player.username === true ? 'bold' : null
                   }
-                >
-                  <th key={index}>{player.username}</th>
-                  <th>{player.wins}</th>
-                  <th>{player.losses}</th>
-                </tr>
-              ))}
-        </div>
-    );
+        >
+          <th key={index}>{player.username}</th>
+          <th>{player.wins}</th>
+          <th>{player.losses}</th>
+        </tr>
+      ))}
+    </div>
+  );
 }
 
 export default StatsComponent;
